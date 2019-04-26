d2f6f60dca6446adbfd0d7d15d0d5ba62bf51a53
Adam Sobieski <adamsobieski@hotmail.com>
2019-04-26 00:38:38 -0400

    Updating

---
+++ Logic/Collections.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ClearScript;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Contracts;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+
+namespace Logic.Collections
+{
+
+}
