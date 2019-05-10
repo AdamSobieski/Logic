d528e841e24860b6a5df72ce040d657f5a983e20
Adam Sobieski <adamsobieski@hotmail.com>
2019-05-09 21:20:40 -0400

    Updating

--- Logic/Argumentation.cs
+++ Logic/Argumentation.cs
@@ -11,55 +11,46 @@ namespace Logic.Argumentation
 {
     public interface _1
     {
-        IReadOnlyList<CompoundExpression> RuleSupport { get; }
-        IReadOnlyList<CompoundExpression> RuleDerived { get; }
-        IBinding Binding { get; }
-        IReadOnlyList<CompoundExpression> Support { get; }
-        IReadOnlyList<CompoundExpression> Derived { get; }
-    }
-
-    public interface _2
-    {
-        IReadOnlyList<CompoundExpression> RuleSupport { get; }
-        CompoundExpression RuleDerived { get; }
-        IBinding Binding { get; }
-        IReadOnlyList<CompoundExpression> Support { get; }
-        CompoundExpression Derived { get; }
-    }
-
-    public interface _3
-    {
         CompoundExpression Rule { get; }
         IBinding Binding { get; }
         CompoundExpression Expression { get; }
     }
 
+    public interface _2
+    {
+        IReadOnlyList<CompoundExpression> RuleSupport { get; }
+        IReadOnlyList<CompoundExpression> RuleDerived { get; }
+        IBinding Binding { get; }
+        IReadOnlyList<CompoundExpression> Support { get; }
+        IReadOnlyList<CompoundExpression> Derived { get; }
+    }
+
+    public interface _3
+    {
+        IReadOnlyList<CompoundExpression> RuleSupport { get; }
+        CompoundExpression RuleDerived { get; }
+        IBinding Binding { get; }
+        IReadOnlyList<CompoundExpression> Support { get; }
+        CompoundExpression Derived { get; }
+    }
+
     public interface _4
     {
-        IReadOnlyList<CompoundExpression> Derived { get; }
-        IReadOnlyList<_4> Support { get; }
-        IBinding Binding { get; }
-        IReadOnlyList<CompoundExpression> RuleDerived { get; }
         IReadOnlyList<CompoundExpression> RuleSupport { get; }
+        CompoundExpression RuleDerived { get; }
+        IBinding Binding { get; }
+        IReadOnlyList<_4> Support { get; }
+        CompoundExpression Derived { get; }
     }
 
     public interface _5
     {
-        CompoundExpression Derived { get; }
-        IReadOnlyList<_5> Support { get; }
-        IBinding Binding { get; }
-        CompoundExpression RuleDerived { get; }
-        IReadOnlyList<CompoundExpression> RuleSupport { get; }
+        CompoundExpression Expression { get; }
+        IReadOnlyList<_6> Supports { get; }
+        IReadOnlyList<_6> Opposes { get; }
     }
 
     public interface _6
-    {
-        CompoundExpression Expression { get; }
-        IReadOnlyList<_7> Supports { get; }
-        IReadOnlyList<_7> Opposes { get; }
-    }
-
-    public interface _7
     {
         IReadOnlyList<CompoundExpression> RuleSupport { get; }
         CompoundExpression RuleDerived { get; }
