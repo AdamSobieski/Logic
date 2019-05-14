477a3e6dfe4af90dfee70b0ecd18af538b511c33
Adam Sobieski <adamsobieski@hotmail.com>
2019-05-14 15:31:02 -0400

    Updating

--- Logic/Argumentation.cs
+++ Logic/Argumentation.cs
@@ -86,7 +86,7 @@ namespace Logic.Argumentation
 
     public interface _10
     {
-        CompoundExpression Expression { get; } // Relation(Source.Expression, Target.Expression)
+        CompoundExpression Expression { get; }
 
         _9 Source { get; }
         PredicateExpression Relation { get; }
@@ -101,7 +101,7 @@ namespace Logic.Argumentation
         CompoundExpression RuleHead { get; }
         IBinding Binding { get; }
         IReadOnlyList<_9> Body { get; }
-        _10 Head { get; } // Head.Expression = Head.Relation(Head.Source.Expression, Head.Target.Expression)
+        _10 Head { get; }
     }
 
     public interface _12
@@ -115,23 +115,99 @@ namespace Logic.Argumentation
 
     public interface _13 : _12
     {
-        //CompoundExpression Expression { get; } // Relation(Source.Expression, Target.Expression)
-        //IReadOnlyList<_13> IncomingEdges { get; }
-        //IReadOnlyList<_13> OutgoingEdges { get; }
-
         _12 Source { get; }
         PredicateExpression Relation { get; }
         _12 Target { get; }
-
-        //IReadOnlyList<_14> SupportedBy { get; }
     }
 
-    public interface _14 // : _12
+    public interface _14
     {
         IReadOnlyList<CompoundExpression> RuleBody { get; }
         CompoundExpression RuleHead { get; }
         IBinding Binding { get; }
         IReadOnlyList<_12> Body { get; }
         _12 Head { get; }
+    }
+
+    public interface _15
+    {
+        CompoundExpression Expression { get; }
+        IReadOnlyList<_16> IncomingEdges { get; }
+        IReadOnlyList<_16> OutgoingEdges { get; }
+
+        IReadOnlyList<_17> SupportedBy { get; }
+        IReadOnlyList<_17> OpposedBy { get; }
+    }
+
+    public interface _16 : _15
+    {
+        _15 Source { get; }
+        PredicateExpression Relation { get; }
+        _15 Target { get; }
+    }
+
+    public interface _17
+    {
+        IReadOnlyList<CompoundExpression> RuleBody { get; }
+        CompoundExpression RuleHead { get; }
+        IBinding Binding { get; }
+        IReadOnlyList<_15> Body { get; }
+        _15 Head { get; }
+    }
+
+    public interface _18
+    {
+        CompoundExpression Expression { get; }
+        IReadOnlyList<_19> IncomingEdges { get; }
+        IReadOnlyList<_19> OutgoingEdges { get; }
+    }
+
+    public interface _19 : _18
+    {
+        _18 Source { get; }
+        PredicateExpression Relation { get; }
+        _18 Target { get; }
+    }
+
+    public interface _20 : _18
+    {
+        IReadOnlyList<CompoundExpression> RuleBody { get; }
+        CompoundExpression RuleHead { get; }
+        IBinding Binding { get; }
+        IReadOnlyList<_18> Body { get; }
+        _18 Head { get; }
+    }
+
+    public interface _21
+    {
+        CompoundExpression Expression { get; }
+    }
+
+    public interface _22 : _21
+    {
+        IReadOnlyList<_23> IncomingEdges { get; }
+        IReadOnlyList<_23> OutgoingEdges { get; }
+    }
+
+    public interface _23 : _21
+    {
+        _22 Source { get; }
+        PredicateExpression Relation { get; }
+        _22 Target { get; }
+    }
+
+    public interface _24 : _21
+    {
+        IReadOnlyList<_25> SupportedBy { get; }
+        IReadOnlyList<_25> OpposedBy { get; }
+    }
+
+    public interface _25
+    {
+        IReadOnlyList<CompoundExpression> RuleBody { get; }
+        CompoundExpression RuleHead { get; }
+        IBinding Binding { get; }
+        IReadOnlyList<_21> Body { get; }
+        _24 Head { get; }
     }
 }
