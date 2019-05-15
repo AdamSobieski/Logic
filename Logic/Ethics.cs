fa20433edf1615ae4bead83a0e8614fd7895a440
Adam Sobieski <adamsobieski@hotmail.com>
2019-05-15 09:33:38 -0400

    Updating

--- Logic/Ethics.cs
+++ Logic/Ethics.cs
@@ -8,9 +8,12 @@ namespace Logic.Ethics
 {
     // https://plato.stanford.edu/entries/reasoning-moral/
     // (1) Ethical reasoning
+    //     e.g. https://sites.google.com/site/acordaplog/implementation
+    //     e.g. https://github.com/merah-putih/qualm
     // (2) Ethical preferences and constraints with respect to planning
     // (3) Ethical reasoning about actions and plans, e.g. plan verification and validation
 
+    // Pereira, Luís Moniz, and Ari Saptawijaya. Programming machine ethics. Vol. 26. Cham: Springer, 2016.
     // Dennis, Louise A., Michael Fisher, Marija Slavkovik, and Matthew P. Webster. "Formal verification of ethical choices in autonomous systems." Robotics and Autonomous Systems 77 (2016): 1-14.
     // Fisher, Michael, Louise A. Dennis, and Matthew P. Webster. "Verifying autonomous systems." Commun. ACM 56, no. 9 (2013): 84-93.
 }
