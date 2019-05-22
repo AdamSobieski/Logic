01a7c2fecf2ea53e9d520920dc32ee7ee25e9aa5
Adam Sobieski <adamsobieski@hotmail.com>
2019-05-22 19:38:18 -0400

    Updating

---
+++ Logic/Collections/ISemanticTree.cs
@@ -0,0 +1,18 @@
+﻿/*********************************************************
+* 
+*  Author:        Adam Sobieski
+*
+*********************************************************/
+
+using Logic.Expressions;
+using System.Collections.Generic;
+
+namespace System.Collections.Specialized
+{
+    public interface ISemanticTree
+    {
+        IList<ISemanticTree> Children { get; }
+
+        IList<CompoundExpression> Semantics { get; }
+    }
+}
