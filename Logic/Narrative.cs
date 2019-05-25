9f4825d2b31808092e11980d302f0b62f254d428
Adam Sobieski <adamsobieski@hotmail.com>
2019-05-24 22:23:50 -0400

    Updating

--- Logic/Narrative.cs
+++ Logic/Narrative.cs
@@ -13,7 +13,7 @@ namespace Logic.Narrative
     // see also: 
     // Cardona-Rivera, Rogelio E., and R. Michael Young. "Desiderata for a Computational Model of Human Online Narrative Sensemaking." (2019).
     //
-    // to do: (1) semantics and emergent semantics through derivation
+    // to do: (1) graph structure, semantics and emergent semantics through automated reasoning processes
     //        (2) hermeneutics, interpretive inferencing and interpretations of: events, sequences of events, and narrative structure
     //            (a) an intelligent agent, utilizing a knowledgebase, can make inferences which include interpretive inferences
     //        (3) the modelling of story characters can be a use case scenario for the intelligent agents architecture
