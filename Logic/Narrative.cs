233fd2eb1c956ed12200776a1640ed7b616615c9
Adam Sobieski <adamsobieski@hotmail.com>
2019-05-30 08:35:53 -0400

    Updating

--- Logic/Narrative.cs
+++ Logic/Narrative.cs
@@ -8,16 +8,16 @@ using Logic.Expressions;
 using System.Collections.Generic;
 using System.Linq;
 
+// see also: 
+// Cardona-Rivera, Rogelio E., and R. Michael Young. "Desiderata for a Computational Model of Human Online Narrative Sensemaking." (2019).
+//
+// to do: (1) graph structure, semantics and emergent semantics through automated reasoning processes
+//        (2) hermeneutics, interpretive inferencing and interpretations of: events, sequences of events, and narrative structure
+//            (a) an intelligent agent, utilizing a knowledgebase, can make inferences which include interpretive inferences
+//        (3) the modelling of story characters can be a use case scenario for the intelligent agents architecture
+
 namespace Logic.Narrative
 {
-    // see also: 
-    // Cardona-Rivera, Rogelio E., and R. Michael Young. "Desiderata for a Computational Model of Human Online Narrative Sensemaking." (2019).
-    //
-    // to do: (1) graph structure, semantics and emergent semantics through automated reasoning processes
-    //        (2) hermeneutics, interpretive inferencing and interpretations of: events, sequences of events, and narrative structure
-    //            (a) an intelligent agent, utilizing a knowledgebase, can make inferences which include interpretive inferences
-    //        (3) the modelling of story characters can be a use case scenario for the intelligent agents architecture
-
     public interface IEvent : IIndividual
     {
         IDelta<CompoundExpression> Effects { get; }
@@ -72,27 +72,4 @@ namespace Logic.Narrative
 
         IList<CompoundExpression> Semantics { get; }
     }
-
-    //public interface IEventGraph : IIndividual
-    //{
-    //    //string Id { get; set; }
-    //    //IEventGraph GetSubgraphById(string id);
-
-    //    IEventGraph Root { get; }
-
-    //    IQueryable<IEvent> Nodes { get; }
-    //    IQueryable<IEventEdge> Edges { get; }
-    //    IQueryable<IEventGraph> Subgraphs { get; }
-
-    //    IEvent FindOrCreateNode(/*...*/);
-    //    IEventEdge FindOrCreateEdge(IEvent source, PredicateExpression relation, IEvent target);
-    //    IEventGraph FindOrCreateSubgraph(IEnumerable<IEventEdge> edges);
-
-    //    bool Add(IEventEdge edge);
-    //    bool Remove(IEventEdge edge);
-    //    bool Replace(IEventEdge remove, IEventEdge add);
-    //    bool Contains(IEventEdge edge);
-
-    //    IList<CompoundExpression> Semantics { get; }
-    //}
 }
