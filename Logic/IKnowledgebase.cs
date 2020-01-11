2f59e8e3f831fa542d43e139f150b4b19cba7a4a
Adam Sobieski <adamsobieski@hotmail.com>
2020-01-11 15:36:32 -0500

    Updating

--- Logic/IKnowledgebase.cs
+++ Logic/IKnowledgebase.cs
@@ -5,12 +5,12 @@ namespace Logic
 {
     public interface IKnowledgebase
     {
-        void AddFunctor(string functor, Type valueType, params Type[] types);
+        void AddFunctor(string functor, Type valueType, params Type[] tupleTypes);
         void RemoveFunctor(string functor);
-        bool Contains(string functor, out object value, params object[] tuple);
         void Store(string functor, object value, params object[] tuple);
+        bool Contains(string functor, out object value, params object[] tuple);
         void Remove(string functor, params object[] tuple);
-        void AddRule(Expression rule);
-        void RemoveRule(Expression rule);
+        void AddRule(string functor, Expression rule);
+        void RemoveRule(string functor, Expression rule);
     }
 }
