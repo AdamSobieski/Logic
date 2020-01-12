39c57a25182795014a90b75735e5122125ca0722
Adam Sobieski <adamsobieski@hotmail.com>
2020-01-11 22:20:38 -0500

    Updating

--- Logic/Collections/IKnowledgebase.cs
+++ Logic/Collections/IKnowledgebase.cs
@@ -8,12 +8,12 @@ namespace Logic.Collections
     {
         void AddFunctor(string functor, Type valueType, params Type[] tupleTypes);
         bool ContainsFunctor(string functor);
-        void RemoveFunctor(string functor);
+        bool RemoveFunctor(string functor);
         void Store(string functor, object value, params object[] tuple);
         bool Contains(string functor, out object value, params object[] tuple);
-        void Remove(string functor, params object[] tuple);
+        bool Remove(string functor, params object[] tuple);
         void AddRule(string functor, Expression rule);
-        void RemoveRule(string functor, Expression rule);
+        bool RemoveRule(string functor, Expression rule);
 
         IQueryable<IRow> GetAssertions(string functor);
         IQueryable<Expression> GetRules(string functor);
