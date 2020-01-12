602e325658f70379a6c16f8fa9b45d25df1c3a9d
Adam Sobieski <adamsobieski@hotmail.com>
2020-01-11 22:12:35 -0500

    Updating

--- Logic/Collections/IKnowledgebase.cs
+++ Logic/Collections/IKnowledgebase.cs
@@ -7,6 +7,7 @@ namespace Logic.Collections
     public interface IKnowledgebase
     {
         void AddFunctor(string functor, Type valueType, params Type[] tupleTypes);
+        bool ContainsFunctor(string functor);
         void RemoveFunctor(string functor);
         void Store(string functor, object value, params object[] tuple);
         bool Contains(string functor, out object value, params object[] tuple);
