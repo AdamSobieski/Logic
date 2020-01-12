ebfb5ee18802e64af350ebc2b442915711b27f63
Adam Sobieski <adamsobieski@hotmail.com>
2020-01-12 02:26:20 -0500

    Updating

---
+++ Logic/Collections/ITupleStore.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Logic.Collections
+{
+    public interface ITupleStore : IEnumerable<IRow>
+    {
+        bool Contains(out object value, params object[] tuple);
+        void Store(object value, params object[] tuple);
+        bool Remove(params object[] tuple);
+        void Clear();
+    }
+}
