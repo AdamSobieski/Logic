1117e991a904fd47f7599b34562cff8e57138cbd
Adam Sobieski <adamsobieski@hotmail.com>
2020-01-14 02:02:13 -0500

    Updating

--- Logic/Rule.cs
+++ Logic/Rule.cs
@@ -4,5 +4,5 @@ using System.Collections.Generic;
 
 namespace Logic
 {
-    public delegate IEnumerable<object> Rule(IKnowledgebase kb, object[] data, Mode mode);
+    public delegate IEnumerable<object> Rule(IKnowledgebase kb, object[] data, JustificationMode mode);
 }
