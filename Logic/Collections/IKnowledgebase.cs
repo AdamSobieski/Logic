e49a599554cbe367c292f682c0118574806c5500
Adam Sobieski <adamsobieski@hotmail.com>
2020-01-16 12:15:54 -0500

    Updating

--- Logic/Collections/IKnowledgebase.cs
+++ Logic/Collections/IKnowledgebase.cs
@@ -6,6 +6,8 @@ namespace Logic.Collections
 {
     public interface IKnowledgebase
     {
+        bool IsScope { get; }
+
         void AddFunctor(string functor, Type valueType, params Type[] tupleTypes);
         void AddFunctor(string functor, IStorage custom);
         bool ContainsFunctor(string functor);
@@ -15,10 +17,14 @@ namespace Logic.Collections
         bool Contains(string functor, out object value, params object[] tuple);
         bool Contains(string functor, out Justification justification, out object value, params object[] tuple);
         bool Remove(string functor, params object[] tuple);
-        
+
         void AddRule(string functor, Rule rule);
         bool ContainsRule(string functor, Rule rule);
         bool RemoveRule(string functor, Rule rule);
+
+        void AddConstraint(Constraint constraint);
+        bool ContainsConstraint(Constraint constraint);
+        bool RemoveConstraint(Constraint constraint);
 
         IEnumerable<Justification> AsEnumerable(string functor, Mode mode);
         IEnumerable<Justification> AsEnumerable(string functor, Mode mode, object[] data, JustificationSettings justification);
